07e959725dc4605a8d926ea85a8b59e3d348a67f
HanHan195 <henrytran.195@gmail.com>
2023-09-15 14:20:05 +0700

    d

---
+++ src/useEffect/ListStudents.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ListStudents() {
+
+    const [student, setStudent] = use
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ListStudents;
